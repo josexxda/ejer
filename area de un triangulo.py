c1f0f1430e7b917883bacaea90b70a1663e2b42e
josexxda <94769787+josexxda@users.noreply.github.com>
2022-01-07 07:38:49 -0600

    Add files via upload

---
+++ area de un triangulo.py
@@ -0,0 +1,5 @@
+#area triangulo
+base = int(input("ingresar la base del triangulo: "))
+altura = int (input("ingresar la altura del triangulo: "))
+area  = (base*altura) / 2
+print("area de un triangulo es :",area,"cm²")
